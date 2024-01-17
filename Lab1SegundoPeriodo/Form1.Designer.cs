52908f59dfd32a9b937842fd6a8563b5b8c744f8
ascencio000 <saraascencio16@gmail.com>
2024-01-17 13:56:48 -0700

    Agregar archivos de proyecto.

---
+++ Lab1SegundoPeriodo/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Lab1SegundoPeriodo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txt_numero = new System.Windows.Forms.TextBox();
+            this.Btn_Calcular = new System.Windows.Forms.Button();
+            this.Btn_Reiniciar = new System.Windows.Forms.Button();
+            this.Btn_Salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cálculo de factorial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese un número:";
+            // 
+            // Txt_numero
+            // 
+            this.Txt_numero.Location = new System.Drawing.Point(171, 54);
+            this.Txt_numero.Name = "Txt_numero";
+            this.Txt_numero.Size = new System.Drawing.Size(100, 22);
+            this.Txt_numero.TabIndex = 2;
+            // 
+            // Btn_Calcular
+            // 
+            this.Btn_Calcular.Location = new System.Drawing.Point(39, 450);
+            this.Btn_Calcular.Name = "Btn_Calcular";
+            this.Btn_Calcular.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Calcular.TabIndex = 3;
+            this.Btn_Calcular.Text = "Calcular";
+            this.Btn_Calcular.UseVisualStyleBackColor = true;
+            this.Btn_Calcular.Click += new System.EventHandler(this.Btn_Calcular_Click);
+            // 
+            // Btn_Reiniciar
+            // 
+            this.Btn_Reiniciar.Location = new System.Drawing.Point(140, 450);
+            this.Btn_Reiniciar.Name = "Btn_Reiniciar";
+            this.Btn_Reiniciar.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Reiniciar.TabIndex = 4;
+            this.Btn_Reiniciar.Text = "Reiniciar";
+            this.Btn_Reiniciar.UseVisualStyleBackColor = true;
+            this.Btn_Reiniciar.Click += new System.EventHandler(this.Btn_Reiniciar_Click);
+            // 
+            // Btn_Salir
+            // 
+            this.Btn_Salir.Location = new System.Drawing.Point(237, 450);
+            this.Btn_Salir.Name = "Btn_Salir";
+            this.Btn_Salir.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Salir.TabIndex = 5;
+            this.Btn_Salir.Text = "Salir";
+            this.Btn_Salir.UseVisualStyleBackColor = true;
+            this.Btn_Salir.Click += new System.EventHandler(this.Btn_Salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 565);
+            this.Controls.Add(this.Btn_Salir);
+            this.Controls.Add(this.Btn_Reiniciar);
+            this.Controls.Add(this.Btn_Calcular);
+            this.Controls.Add(this.Txt_numero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txt_numero;
+        private System.Windows.Forms.Button Btn_Calcular;
+        private System.Windows.Forms.Button Btn_Reiniciar;
+        private System.Windows.Forms.Button Btn_Salir;
+    }
+}
+
---
+++ Lab1SegundoPeriodo/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1SegundoPeriodo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        TextBox[] arreglo;
+        Label la1 = new Label();
+        Label la2 = new Label();
+        Label la3 = new Label();
+        Label la4 = new Label();
+        Label la5 = new Label();
+
+        private void Btn_Calcular_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                int numero1 = 0, potencia = 1;
+                int mayor = 0, comparacion = 0, menor = 0, sumatoria = 0, ultimo = 0, penultimo = 0;
+                numero1 = Convert.ToInt32(Txt_numero.Text);
+                string nombre = " ";
+                if (numero1 < 0)
+                {
+                    MessageBox.Show("El número ingresado es negativo", "Aviso");
+                    this.Close();
+                }
+
+                int x = 20, y = 120;
+                arreglo = new TextBox[numero1];
+                for(int i = 0; i < arreglo.Length; i++)
+                {
+                    x += 30;
+                    nombre = "texto" + i;
+                    arreglo[i] = new TextBox();
+                    arreglo[i].Name = nombre;
+                    arreglo[i].Height = 10;
+                    arreglo[i].Width = 20;
+                    arreglo[i].Location = new Point(x, y);
+                    this.Controls.Add(arreglo[i]);
+
+                }
+
+                int contador = 1;
+                for (int j = 0; j < arreglo.Length; j++)
+                {
+                    arreglo[j].Text = contador.ToString();
+                    contador++;
+                }
+
+                int multi = 1;
+                for (int h = 0; h < arreglo.Length; h++)
+                {
+                    multi = multi * Convert.ToInt32(arreglo[h].Text);
+                    sumatoria = sumatoria + Convert.ToInt32(arreglo[h].Text);
+                    ultimo = Convert.ToInt32(arreglo[arreglo.Length - 1].Text);
+                    penultimo = Convert.ToInt32(arreglo[arreglo.Length - 2].Text);
+                }
+
+                
+                for (int g = 0;g  < arreglo.Length; g++)
+                {
+                    comparacion = Convert.ToInt32(arreglo[g].Text);
+                    menor = Convert.ToInt32(arreglo[0].Text);
+                    if (comparacion > mayor)
+                    {
+                        mayor = comparacion;
+                    }else if (comparacion < menor)
+                    {
+                        menor = comparacion;
+                    }
+
+
+                }
+
+                for(int d = 1; d <= penultimo; d++)
+                {
+                    if (d == 1)
+                    {
+                        potencia = potencia * ultimo;
+                    } else
+                    {
+                        potencia = potencia * ultimo;
+                    }
+                   
+
+                }
+
+
+
+
+
+
+
+              
+                la1.Width = 200;
+                la1.Height = 20;
+                la1.Location = new Point(x/2, y+50);
+                la1.Text = "El resultado es "+multi.ToString();
+                this.Controls.Add(la1);
+
+                
+                la2.Width = 200;
+                la2.Height = 20;
+                la2.Location = new Point(x / 2, y + 75);
+                la2.Text = "El mayor es " + mayor.ToString();
+                this.Controls.Add(la2);
+
+
+            
+                la3.Width = 200;
+                la3.Height = 20;
+                la3.Location = new Point(x / 2, y + 100);
+                la3.Text = "El menor es " + menor.ToString();
+                this.Controls.Add(la3);
+
+            
+                la4.Width = 200;
+                la4.Height = 20;
+                la4.Location = new Point(x / 2, y + 125);
+                la4.Text = "La sumatoria es " + sumatoria.ToString();
+                this.Controls.Add(la4);
+
+
+         
+                la5.Width = 200;
+                la5.Height = 20;
+                la5.Location = new Point(x / 2, y + 150);
+                la5.Text = "La potencia es " + potencia.ToString();
+                this.Controls.Add(la5);
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("El número ingresado es un carácter o un decimal", "Aviso");
+            }
+                
+
+
+
+           
+          
+            
+        }
+
+        private void Btn_Reiniciar_Click(object sender, EventArgs e)
+        {
+
+            if (arreglo != null)
+            {
+                for(int i = 0;i < arreglo.Length;i++)
+                {
+
+                    this.Controls.Remove(arreglo[i]);
+
+                }
+
+                this.Controls.Remove(la1);
+                this.Controls.Remove(la2);
+                this.Controls.Remove(la3);
+                this.Controls.Remove(la4);
+                this.Controls.Remove(la5);
+                Txt_numero.Clear();
+                Txt_numero.Focus();
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
